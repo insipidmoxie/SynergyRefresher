7c56b5341d402f7bf88f81063208181e4d646d70
aakkt <67505590+aakkt@users.noreply.github.com>
2020-06-30 15:06:37 +0100

    Add files via upload
    Now uses two tabs instead of two windows
    Plays a sound on record detection
    Comments improved along with general clarity improvements.

--- synergy_refresher_public.py
+++ synergy_refresher_public.py
@@ -1,5 +1,5 @@
 #Synergy Refresher by Reece Griffith
-#Future updates to include a GUI and system notification on record detection.
+#Future updates to include a GUI with pause&resume buttons and windows notification on record detection.
 #Future update where it refreshes as soon as the button appears is also easily possible but it's set to click every 2 seconds for now.
 #Currently the script logs into synergy and CTAS and constantly checks for cases. When a case is detected the script will play a sound and stop.
 
@@ -15,70 +15,92 @@ import sys
 import winsound
 
 
-synergy_url = "https://ttt3.callscripter.com/"
-synergy_username = "replace this text with your synergy username"
-synergy_password = "replace this text with your synergy password"
-ctas_userame = "replace this text with your ctas username"
-ctas_password = "replace this text with your ctas password"
+synergy_username = "replace this text with your synergy username" #PLEASE ENTER YOUR SYNERGY USERNAME BETWEEN THESE QUOTATION MARKS
+synergy_password = "replace this text with your synergy password" #PLEASE ENTER YOUR SYNERGY PASSWORD BETWEEN THESE QUOTATION MARKS
+ctas_userame = "replace this text with your ctas username"        #PLEASE ENTER YOUR CTAS USERNAME BETWEEN THESE QUOTATION MARKS
+ctas_password = "replace this text with your ctas password"       #PLEASE ENTER YOUR CTAS PASSWORD BETWEEN THESE QUOTATION MARKS
+
+
+
+
+
+
+
+
+
+#main code below here. Do NOT edit below here unless you want to change how many times the alert sound plays.
+
+
 ctas_url = "https://contact-tracing-staff.phe.gov.uk/staff/sign_in"
-    
-def alert():                                                        #plays the alert.wav sound thrice. To change to once or twice delete two or one of these lines, respectively.
-    winsound.PlaySound('alert.wav', winsound.SND_FILENAME)
+synergy_url = "https://ttt3.callscripter.com/"
+
+
+def open_CTAS_tab():                                                #opens a tab for CTAS
+    driver.execute_script("window.open('{}')".format(ctas_url))
+    return
+
+def focus_synergy_tab():                                            #switches to the synergy tab
+    driver.switch_to.window(driver.window_handles[0])
+    return
+
+def alert():                                                        #plays the alert.wav sound thrice. 
+    winsound.PlaySound('alert.wav', winsound.SND_FILENAME)          #To change to once or twice delete two or one of these lines, respectively.
     winsound.PlaySound('alert.wav', winsound.SND_FILENAME)
     winsound.PlaySound('alert.wav', winsound.SND_FILENAME)
     return
 
 def Login_to_Synergy(username, password):
-    synergy_driver.find_element(By.CSS_SELECTOR,"#txtUsername").send_keys(username + Keys.TAB)      #enters username
-    synergy_driver.find_element(By.CSS_SELECTOR,"#txtPassword").send_keys(password)                 #enters password
-    WebDriverWait(synergy_driver, timeout=1)                                                        
-    synergy_driver.find_element(By.CSS_SELECTOR,".btn").click()                                     #clicks login
+    driver.find_element(By.CSS_SELECTOR,"#txtUsername").send_keys(username + Keys.TAB)      #enters username
+    driver.find_element(By.CSS_SELECTOR,"#txtPassword").send_keys(password)                 #enters password
+    WebDriverWait(driver, timeout=1)                                                        
+    driver.find_element(By.CSS_SELECTOR,".btn").click()                                     #clicks login
     return
 
 def Login_to_CTAS(username, password):
-    ctas_driver.find_element(By.CSS_SELECTOR, "#staff-member-email-field").send_keys(username + Keys.TAB)       #enters username
-    ctas_driver.find_element(By.CSS_SELECTOR, "#staff-member-password-field").send_keys(password + Keys.ENTER)  #enters password
+    driver.find_element(By.CSS_SELECTOR, "#staff-member-email-field").send_keys(username + Keys.TAB)       #enters username
+    driver.find_element(By.CSS_SELECTOR, "#staff-member-password-field").send_keys(password + Keys.ENTER)  #enters password
     return
 
 def start_tracing():
-    iframe = synergy_driver.find_element(By.CSS_SELECTOR, "#ifFrontpage")                           
-    synergy_driver.switch_to.frame(iframe)
-    iframe3 = synergy_driver.find_element(By.CSS_SELECTOR, ".stack-fill > iframe:nth-child(1)")
-    synergy_driver.switch_to.frame(iframe3)
-    synergy_driver.find_element(By.TAG_NAME, "button").click()                                          #clicks start tracing
-    synergy_driver.switch_to.default_content()
+    iframe = driver.find_element(By.CSS_SELECTOR, "#ifFrontpage")                           
+    driver.switch_to.frame(iframe)
+    iframe3 = driver.find_element(By.CSS_SELECTOR, ".stack-fill > iframe:nth-child(1)")
+    driver.switch_to.frame(iframe3)
+    driver.find_element(By.TAG_NAME, "button").click()                                          #clicks start tracing
+    driver.switch_to.default_content()
     return
 
 
 def back():
-    iframe = synergy_driver.find_element(By.CSS_SELECTOR, "#ifFrontpage")
-    synergy_driver.switch_to.frame(iframe)
-    records_text = synergy_driver.find_element(By.CSS_SELECTOR, "div.CSGroup:nth-child(3)").text
-    if "no available records for you" in records_text:                                                  #checks if there are no records
-        synergy_driver.find_element(By.CSS_SELECTOR, "#backToCampaignsBtn").click()                     #if no records click back
-        synergy_driver.switch_to.default_content()
+    iframe = driver.find_element(By.CSS_SELECTOR, "#ifFrontpage")
+    driver.switch_to.frame(iframe)                                                              #focuses on frame so it can see the page
+    records_text = driver.find_element(By.CSS_SELECTOR, "div.CSGroup:nth-child(3)").text
+    if "no available records for you" in records_text:                                          #checks if there are no records
+        driver.find_element(By.CSS_SELECTOR, "#backToCampaignsBtn").click()                     #if no records click back
+        driver.switch_to.default_content()
         return
     else:
         alert()
-        sys.exit("You probably have a record.")                                                         #if a record is there the script plays a sound and stops
+        sys.exit("You probably have a record.")                                                 #if a record is there the script plays a sound and stops
 
-if __name__ == "__main__":
-    synergy_driver = webdriver.Firefox()    #starts a browser for synergy
-    ctas_driver = webdriver.Firefox()       #starts a browser for ctas 
+if __name__ == "__main__":                                  #main loop
     
-    synergy_driver.get(synergy_url)         #loads synergy
-    ctas_driver.get(ctas_url)               #loads ctas 
-    
+    driver = webdriver.Firefox()                            #starts a browser for synergy
+    driver.get(synergy_url)                                 #loads synergy   
     Login_to_Synergy(synergy_username, synergy_password)    #logs into synergy
+    open_CTAS_tab()                                         #opens tab for ctas
+    time.sleep(1)
     Login_to_CTAS(ctas_userame, ctas_password)              #logs into ctas
+    time.sleep(1)
+    focus_synergy_tab()                                     #switches back to synergy tab
     time.sleep(2)
     
     
-    while(1):                   #repeats the following until stopped:
-        start_tracing()         #hits start tracing
-        time.sleep(2)           #waits 2 seconds
-        back()                  #hits back if no case
-        time.sleep(2)           #waits 2 seconds
+    while(1):                                               #repeats the following until stopped:
+        start_tracing()                                     #hits start tracing
+        time.sleep(2)                                       #waits 2 seconds
+        back()                                              #hits back if no case
+        time.sleep(2)                                       #waits 2 seconds
         
 
 
